e72a3edf6344616c6235fd8d04b7df1c0ade830b
peyangu-japan <peyangujp@gmail.com>
2024-11-03 22:59:55 +0900

    index.js

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["discord.gg/bjJTRhpJuf"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
-let currentTypeIndex = 0;
+let currentTypeIndex = 1;
 
 async function login() {
   try {
